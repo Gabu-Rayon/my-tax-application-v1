27ce6f1eeaab98c162439efd45a01624adea0db2
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-10-29 18:16:53 +0300

    Commit

--- user-sidebar.php
+++ user-sidebar.php
@@ -2,7 +2,9 @@
     <ul>
     <a href="dashboard.php"><button class="btn btn-danger"><?php echo LANG_VALUE_89; ?></button></a>
     <a href="user-profile-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_117; ?></button></a>
+        <a href="user-billing-shipping-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_88; ?></button></a>
         <a href="user-password-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_99; ?></button></a>
+        <a href="user-order.php"><button class="btn btn-danger"><?php echo LANG_VALUE_24; ?></button></a>
         <a href="logout.php"><button class="btn btn-danger"><?php echo LANG_VALUE_14; ?></button></a>
     </ul>
 </div>

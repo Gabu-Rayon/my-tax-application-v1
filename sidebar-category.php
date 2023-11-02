0a5e7f70ff23eac8a55ae5ff6ec26ed43fd829bc
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-11-02 11:02:20 +0300

    Commit

--- sidebar-category.php
+++ sidebar-category.php
@@ -3,6 +3,7 @@
 
     <ul id="menu-group-1" class="nav menu">
         <?php
+        //Get all the categories
         $i = 0;
         // $statement = $pdo->prepare("SELECT * FROM tbl_top_category WHERE show_on_menu=1");
         $statement = $pdo->prepare("SELECT * FROM tbl_top_category ");
